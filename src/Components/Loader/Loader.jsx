e7db3fd0f4fe1893f88269961a586d85105e5e2d
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-06-13 00:53:39 +0500

    Update Loader.jsx

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -5,8 +5,10 @@ import loader from "../../Assets/loader.gif";
 function Loader() {
   return (
     <div className="loader">
-      <img src={loader} alt="" className="loader-gif" />
-      <img src={img} alt="" className="loader-img" />
+      <div className="loader-div">
+        <img src={loader} alt="" className="loader-gif" />
+        <img src={img} alt="" className="loader-img" />
+      </div>
     </div>
   );
 }
